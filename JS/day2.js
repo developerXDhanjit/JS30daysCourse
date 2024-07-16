a9bd151c2c74e22abce58fb9472cd908b0065086
developerXDhanjit <developerdhanjit@gmail.com>
2024-07-16 21:59:44 +0530

    Day 2 commit

---
+++ JS/day2.js
@@ -0,0 +1,92 @@
+/* Operators  */
+
+/* Activity 1 : Aritmetic operators  */
+
+/* let a = 3 
+let b = 5 
+let sumOfAAndB = a+b 
+console.log(sumOfAAndB); */
+
+/* let a = 10
+let b = 8
+let c = a - b 
+console.log(c); */
+
+/* let a = 6 
+let b = 9 
+let c = a*b 
+console.log(c); */
+
+/* let a = 10 
+let b = 5 
+let c = a / b 
+console.log(c); */
+
+/* let a = 10 
+let b = 3 
+let c = a % b 
+console.log(c); */
+
+/* Activity 2  */
+
+/* let a = 10 
+a += 5 
+console.log(a);
+
+let b = 11 
+b -= 7 
+console.log(b); */
+
+/* Activity 3  */
+
+/* let a = 10 
+let b = 11 
+let c = 10 
+
+console.log(a == b );
+console.log(a == c );
+console.log(b != c); */
+
+/* let num1 = 10 
+let num2 = 11 
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+ */
+
+/* let n = 81 
+let m = 19 
+console.log(n <= m );
+console.log(n >= m ); */
+
+/* Activity 4  */
+/* 
+let a = 11
+let b = 10
+
+let case1 = a > b // true
+let case2 = a < b // false 
+
+let state1 = case1 && case2
+// true and false => false 
+let state2 = case1 || case2
+// true or false => true  
+
+console.log(state1); // false 
+console.log(state2); // true 
+
+let state3 = !state1 // not false  = true  
+let state4 = !state2 // not true = false 
+
+console.log(state3, state4); */
+
+/* Activity 5 {ternary operator } */
+
+/* let check;
+let num = -10
+check = (num > 0) ? "Positive" : "Negative";
+console.log(check);
+ */
+
+
+/* Feature Request  */
